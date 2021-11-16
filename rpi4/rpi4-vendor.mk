db5f672431edc69a3f43c2fc7b4fea05ffc857ec
maxwen <max.weninger@gmail.com>
2021-11-16 15:28:13 +0100

    rpi4: update widevine blob
    add lib64/libwvdrmengine.so and remove obsolete clearkey libs
    
    Change-Id: I809a7506a71ba89bb4c1bc0bb6848087a96a7ffa

--- rpi4/rpi4-vendor.mk
+++ rpi4/rpi4-vendor.mk
@@ -19,6 +19,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 PRODUCT_COPY_FILES += \
     vendor/brcm/rpi4/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:$(TARGET_COPY_OUT_VENDOR)/lib/mediadrm/libwvdrmengine.so \
+    vendor/brcm/rpi4/proprietary/vendor/lib64/mediadrm/libwvdrmengine.so:$(TARGET_COPY_OUT_VENDOR)/lib64/mediadrm/libwvdrmengine.so \
     vendor/brcm/rpi4/proprietary/vendor/bin/hw/android.hardware.drm@1.3-service.widevine:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.drm@1.3-service.widevine \
     vendor/brcm/rpi4/proprietary/vendor/etc/init/android.hardware.drm@1.3-service.widevine.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.drm@1.3-service.widevine.rc \
     vendor/brcm/rpi4/proprietary/vendor/lib64/libwvhidl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libwvhidl.so
